4f383aff945d0e89041656333a6cf363063e0bd1
sepehr-design <sepehr.asemani80@gmail.com>
2025-08-13 18:40:53 +0330

    coding json

--- js/index.js
+++ js/index.js
@@ -4,9 +4,16 @@
 
 
 
-fetch("https://github.com/sepehr-design/main-page/blob/master/project.json")
+fetch("project.json")
     .then(res => res.json())
     .then(data => {
-
-
-    })
+        let output = "<ul>"
+        data.map(item => {
+            console.log(item);
+            output += `<li>${item}</li>`;
+        })
+        output+="</ul>"
+        console.log(output)
+    }).catch(error => {
+    console.error('Error fetching or parsing JSON:', error);
+});
